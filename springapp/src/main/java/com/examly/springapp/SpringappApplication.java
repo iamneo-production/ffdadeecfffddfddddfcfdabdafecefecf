dbce2089db42362f7a3d0897c3f3de2901e9844a
Iamneo <techadmin@iamneo.ai>
2023-08-07 07:21:55 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -6,8 +6,52 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringappApplication {
 
+	FirefoxDriver driver;
+		
+		String iamneoUrl = "http://iamneo.ai";
+		String facebookUrl = "https://www.facebook.com";
+
+		public void invokeBrowser() {
+
+			System.setProperty("webdriver.gecko.driver",
+					"C:\\Users\\SHYAD ANWAR\\OneDrive\\Desktop\\Driver\\firefox\\geckodriver.exe");
+			driver = new FirefoxDriver();
+
+			driver.manage().window().maximize();
+
+
+			driver.get(iamneoUrl);
+
+
+		}
+
+		public void navigateCommands() {
+			driver.navigate().to(facebookUrl);
+
+			String currentUrl = driver.getCurrentUrl();
+			driver.navigate().back();
+			
+			String urlFromwebpage = driver.getCurrentUrl();
+			System.out.println(urlFromwebpage);
+			
+
+			driver.navigate().forward();
+
+			driver.navigate().refresh();
+		}
+
+		public void closeBrowser(){
+			driver.close();
+			
+		}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
+		ASpringappApplication selenium = new SpringappApplication();
+
+			com.examly.springapp.invokeBrowser();
+			com.examly.springapp.navigateCommands();
+			com.examly.springapp.closeBrowser();
 	}
 
 }
